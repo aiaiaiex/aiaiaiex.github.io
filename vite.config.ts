ce01b8c9a4a957a1449d6bc6d891f60cade5eddc
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-30 02:11:20 +0800

    Configure @svgr/plugin-svgo

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,42 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+        svgoConfig: {
+          plugins: [
+            {
+              // https://svgo.dev/docs/preset-default/
+              name: "preset-default",
+              params: {
+                overrides: {
+                  cleanupIds: {
+                    // Do not minify IDs to prevent them from being similar.
+                    // See more:
+                    // https://svgo.dev/docs/plugins/cleanupIds/
+                    minify: false,
+                  },
+                },
+              },
+            },
+            // Remove these since we are inlining SVGs.
+            // See more:
+            // https://svgo.dev/docs/plugins/removeXMLNS/
+            // https://svgo.dev/docs/plugins/removeXlink/
+            "removeXMLNS",
+            {
+              name: "removeXlink",
+              params: {
+                includeLegacy: true,
+              },
+            },
+          ],
+        },
+      },
+    }),
+  ],
 });
