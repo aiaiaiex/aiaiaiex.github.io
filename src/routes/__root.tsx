558c21acf45ed10015e887b34549baa9e4678167
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-03-30 14:42:51 +0800

    Add footer

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const RootLayout = () => (
-  <>
+  <div>
     <div>
       <Link to="/">Home</Link> <Link to="/about">About</Link>
       <Link to="/projects">Projects</Link>
@@ -10,8 +10,12 @@ const RootLayout = () => (
     </div>
     <hr />
     <Outlet />
+    <hr />
+    <div>
+      <span>&#169; 2026 Carlos Jose Ysais</span>
+    </div>
     <TanStackRouterDevtools />
-  </>
+  </div>
 );
 
 export const Route = createRootRoute({ component: RootLayout });
