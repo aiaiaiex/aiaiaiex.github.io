12955b1763705967c5c17a983220aabc3e0db32d
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-04-04 23:27:42 +0800

    Remove unnecessary utility classes

--- src/components/Pending.tsx
+++ src/components/Pending.tsx
@@ -1,6 +1,6 @@
 function Pending() {
   return (
-    <span className="flex animate-pulse items-center justify-center text-center text-8xl font-black text-red-700 sm:text-9xl">
+    <span className="animate-pulse text-center text-8xl font-black text-red-700 sm:text-9xl">
       Loading...
     </span>
   );
