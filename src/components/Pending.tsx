c16112e6e9b1124fc569f7bea3330b306cad9a51
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-04-04 22:32:33 +0800

    Create Pending

---
+++ src/components/Pending.tsx
@@ -0,0 +1,9 @@
+function Pending() {
+  return (
+    <span className="flex animate-pulse items-center justify-center text-center text-8xl font-black text-red-700 sm:text-9xl">
+      Loading...
+    </span>
+  );
+}
+
+export default Pending;
