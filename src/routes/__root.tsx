217ec28f51b565ba1ebf4d0bc1cdc8fd97084f47
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-04-05 01:10:05 +0800

    Add notFoundComponent

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,9 @@
 import { createRootRoute } from "@tanstack/react-router";
 
+import NotFound from "../components/NotFound";
 import RootLayout from "../components/routes/RootLayout";
 
-export const Route = createRootRoute({ component: RootLayout });
+export const Route = createRootRoute({
+  component: RootLayout,
+  notFoundComponent: NotFound,
+});
