8c62c226a3f9b52a5d81041421347e50fc7ddf97
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2026-04-04 17:52:01 +0800

    Create Fallback for Suspense component

---
+++ src/components/Fallback.tsx
@@ -0,0 +1,21 @@
+import type { ComponentProps } from "react";
+
+type FallbackProps = Pick<Required<ComponentProps<"span">>, "className"> & {
+  overrideClassName?: boolean;
+};
+
+function Fallback({ className, overrideClassName = false }: FallbackProps) {
+  return (
+    <span
+      className={
+        overrideClassName
+          ? className
+          : `${className} flex animate-pulse items-center justify-center font-black text-red-700`
+      }
+    >
+      Loading...
+    </span>
+  );
+}
+
+export default Fallback;
